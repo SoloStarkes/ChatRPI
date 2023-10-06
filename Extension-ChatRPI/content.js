1861183ef16b41c3263104e06c947dd6d201dca4
Sthiam915 <Sthiam915@gmail.com>
2023-10-06 19:29:57 -0400

    cleaned the popup a little bit

--- Extension-ChatRPI/content.js
+++ Extension-ChatRPI/content.js
@@ -1,6 +1,7 @@
 var sitebody = document.body;
 
 var popup = document.createElement('span');
+popup.id = "draggableElement"; // Assign the ID "draggableElement"
 
 //making the style
 popup.style.position = "absolute"
@@ -11,24 +12,27 @@ popup.style.left = "10%";
 popup.style.backgroundColor = "white";
 popup.style.borderRadius = "15px";
 popup.style.boxShadow = "5px 0px 10px black"
+popup.style.overflowY = "auto";
 
 //making text content for popup
 var text = document.createElement('span');
 text.style.position = "absolute";
 text.style.width = "80%";
 text.style.height = "10%";
-text.style.top = "20%";
+text.style.top = "10%";
 text.style.left = "10%";
 text.innerText = "Ask me a question.";
 
 //making response text content for popup
 var response = document.createElement('span');
+response.style.border = "1px solid rgb(200,200,200)";
 response.style.position = "absolute";
 response.style.width = "80%";
-response.style.height = "60%";
-response.style.top = "40%";
+response.style.height = "20%";
+response.style.top = "30%";
 response.style.left = "10%";
 response.innerText = "";
+response.style.overflowY = "auto";
 
 //making text box for popup
 var textbox = document.createElement("input");
@@ -64,6 +68,37 @@ sitebody.appendChild(popup);
 
 
 
+var popup = document.getElementById("draggableElement");
+var isDragging = false;
+var offsetX, offsetY;
+
+// Function to handle the mouse down event
+function onMouseDown(event) {
+  isDragging = true;
+  offsetX = event.clientX - popup.getBoundingClientRect().left;
+  offsetY = event.clientY - popup.getBoundingClientRect().top;
+}
+
+// Function to handle the mouse move event
+function onMouseMove(event) {
+  if (isDragging) {
+    var newX = event.clientX - offsetX;
+    var newY = event.clientY - offsetY;
+
+    popup.style.left = newX + "px";
+    popup.style.top = newY + "px";
+  }
+}
+
+// Function to handle the mouse up event
+function onMouseUp() {
+  isDragging = false;
+}
+
+// Add event listeners for mouse events
+popup.addEventListener("mousedown", onMouseDown);
+document.addEventListener("mousemove", onMouseMove);
+document.addEventListener("mouseup", onMouseUp);
 
 
 
